4fe1d4d06d49172345a4c017704dcaa4e967a669
exepirit <exepir1t@protonmail.com>
2023-08-19 00:04:50 +0700

    feat: generate fake data

---
+++ cmd/fill-data/generator.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"github.com/brianvoe/gofakeit"
+	"github.com/exepirit/report-search/internal/data"
+	"github.com/google/uuid"
+	"time"
+)
+
+func IterateGeneratedReports(count int, cb func(report data.Report)) {
+	for i := 0; i < count; i++ {
+		report := GenerateReport()
+		cb(report)
+	}
+}
+
+func GenerateReport() data.Report {
+	return data.Report{
+		ID:        uuid.New(),
+		SubjectID: uuid.New(),
+		Period:    GenerateReportPeriod(),
+		Author:    GenerateUser(),
+		Parts:     GenerateReportParts(),
+	}
+}
+
+func GenerateReportParts() []data.ReportPart {
+	parts := make([]data.ReportPart, gofakeit.Number(1, 20))
+	for i := 0; i < len(parts); i++ {
+		parts[i] = GenerateReportPart()
+	}
+	return parts
+}
+
+func GenerateReportPart() data.ReportPart {
+	return data.ReportPart{
+		ID: uuid.New(),
+		Content: gofakeit.Paragraph(
+			gofakeit.Number(1, 4),  // paragraphCount
+			gofakeit.Number(1, 5),  // sentenceCount
+			gofakeit.Number(3, 15), // wordCount
+			".",                    // separator
+		),
+	}
+}
+
+func GenerateReportPeriod() data.ReportPeriod {
+	startDate := gofakeit.Date()
+	finishDate := startDate.AddDate(0, 0, 7).Add(-time.Second)
+	deadline := gofakeit.DateRange(startDate, finishDate)
+	return data.ReportPeriod{
+		ID:         uuid.New(),
+		StartDate:  startDate,
+		FinishDate: finishDate,
+		Deadline:   deadline,
+	}
+}
+
+func GenerateUser() data.User {
+	return data.User{
+		ID:        int(gofakeit.Int16()),
+		ShortName: ShortenName(gofakeit.FirstName(), gofakeit.LastName()),
+	}
+}
+
+func ShortenName(firstName string, lastName string) string {
+	abbr := firstName[0]
+	return fmt.Sprintf("%s %c.", lastName, abbr)
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/exepirit/report-search
+
+go 1.21
+
+require (
+	github.com/brianvoe/gofakeit v3.18.0+incompatible
+	github.com/google/uuid v1.3.0
+	github.com/typesense/typesense-go v0.8.0
+)
+
+require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/deepmap/oapi-codegen v1.12.3 // indirect
+	github.com/sony/gobreaker v0.5.0 // indirect
+)
